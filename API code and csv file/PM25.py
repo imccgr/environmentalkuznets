c2f321800f04b74776961dd4e32b2de0af7d50d5
HassZ1 <hassamzah@gmail.com>
2022-12-06 11:25:26 +0000

    Fixing pm2.5 error

--- API code and csv file/PM25.py
+++ API code and csv file/PM25.py
@@ -1,26 +1,32 @@
 import requests
 import pandas as pd
 import numpy as np
-rjson = requests.get("https://ghoapi.azureedge.net/api/SDGPM25").json()
 
-# Using list comprehension tool and vectorised operations, a lot simpler to change to other town,urban etc, just change the + 1 index in the rjson value if we wanted in the future
+#Using list comprehension tool and vectorised operations, a lot simpler to change to other town,urban etc, just change the + 1 index in the rjson value if we wanted in the future
+#Used the filtered API now
+
+rjson_2 = requests.get("https://ghoapi.azureedge.net/api/SDGPM25?$filter=Dim1%20eq%20%27TOTL%27").json()
+
+def avg_for_each_country():
+    total_list_of_avg_2010_to_2019 = [np.round(np.mean([rjson_2["value"][(i * 10) + current_year]["NumericValue"] for current_year in range(10)]), decimals = 5) for i in range(189)]
+    print(total_list_of_avg_2010_to_2019)
+    return(total_list_of_avg_2010_to_2019)
+total_list_of_avg_2010_to_2019 = avg_for_each_country()
+
+#9,450 data points for 189 countries. 9450/189=50. 50 data points per country / 5 locations per country = 10 years per country. Therefore no missing data.
+#nvm, there are 195 countries
+
+def total_each_year():
+    total_list_of_each_year = [[rjson_2["value"][(i * 10) + current_year]["NumericValue"] for i in range(195)] for current_year in range(10)]
+    return(total_list_of_each_year)
+total_list_of_each_year = total_each_year()
 
 def listing_countries():
-    country_list = [(rjson["value"][(i * 51) - i]["SpatialDim"]) for i in range(189)]
+    country_list = [(rjson_2["value"][i * 10]["SpatialDim"]) for i in range(195)]
     return(country_list)
 country_list = listing_countries()
 
-def total_for_each_country():
-    total_list_of_avg_2010_to_2019 = [np.round(np.mean([rjson["value"][((i * 51) - i + 1) + (current_year * 5)]["NumericValue"] for current_year in range(10)]), decimals = 5) for i in range(189)]
-    return(total_list_of_avg_2010_to_2019)
-total_list_of_avg_2010_to_2019 = total_for_each_country()
-
-def city_for_each_country():
-    city_list_of_avg_2010_to_2019 = [np.round(np.mean([rjson["value"][((i * 51) - i) + (current_year * 5)]["NumericValue"] for current_year in range(10)]), decimals = 5) for i in range(189)]
-    return(city_list_of_avg_2010_to_2019)
-city_list_of_avg_2010_to_2019 = city_for_each_country()
-
-dict = {"Country:": country_list, "City Average PM2.5:": city_list_of_avg_2010_to_2019, "Total Average PM2.5:": total_list_of_avg_2010_to_2019}
+dict = {"Country:": country_list, "2010 Total:": total_list_of_each_year[0], "2011 Total:": total_list_of_each_year[1], "2012 Total:": total_list_of_each_year[2], "2013 Total:": total_list_of_each_year[3], "2014 Total:": total_list_of_each_year[4], "2015 Total:": total_list_of_each_year[5],"2016 Total:": total_list_of_each_year[6], "2017 Total:": total_list_of_each_year[7], "2018 Total:": total_list_of_each_year[8], "2019 Total:": total_list_of_each_year[9]}
 df = pd.DataFrame(dict)
 df.to_csv('PM25_data.csv')
 print(df)
